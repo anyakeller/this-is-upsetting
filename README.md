ddcd8d8e92b12a157e917801a725ef37af65c2df
Anya L. Keller <kelleranyal@gmail.com>
2021-10-29 00:05:03 -0400

    Did enough in main.  Time to branch out

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # this-is-upsetting
 Diff Eqs are Upsetting
+
+I'm probably not going to finish this FYI
---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+function Nav() {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          Yeet
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
